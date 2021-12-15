fdf95c381ca9dc3e503b19d30c716100a0714018
luis fernando pacco condori <fernandopacco98@hotmail.com>
2021-12-14 20:11:02 -0500

    subiendo las apis correctos

--- apiConDjango/apiConDjango/settings.py
+++ apiConDjango/apiConDjango/settings.py
@@ -106,16 +106,22 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:4200",
+    "http://localhost:4200",
 ]
-CORS_ALLOW_ALL_ORIGINS: bool
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:4200',
+]
+#CORS_ALLOW_ALL_ORIGINS= True 
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
     "OPTIONS",
     "PATCH",
     "POST",
-    "PUT",
+    "PUT",      
 ]
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
